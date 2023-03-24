e7480dd2f2f63901ecb2443a5f83b882574416d3
Luigi <122436095+Luigi-C22@users.noreply.github.com>
2023-03-24 18:37:39 +0100

    Add files via upload

---
+++ W1-P2.js
@@ -0,0 +1,65 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Se sei in difficoltà puoi chiedere aiuto a un Teaching Assistant
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per farlo puoi utilizzare il terminale Bash, quello di VSCode o quello del tuo sistema operativo (se utilizzi macOS o Linux)
+*/
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+
+/* I principali DataType in JavaScript sono: number, string, boolean, undefined, null; costituiscono la parte che viene inserita all'interno
+di un contenitore (la variabile) che è stata precedentemente dichiarata.
+il 'number' è un valore numerico con cui si eseguono operazini aritmetiche
+la 'string' invece è una sequenza di caratteri alfabetici (ma anche numeri considerati come lettere) con funzioni più simili alla grammatica;
+il 'boolean' rappresenta una decisione che può essere vera oppure falsa;
+ */
+
+/* ESERCIZIO 2
+ Descrivi cos'è un oggetto in JavaScript, con parole tue.
+*/
+
+/* Un oggetto in JavaScript viene rappresentato tra parentesi che contengono una serie di informazioni (o proprietà)
+ che appartengono ad un elemento e lo descrivono*/
+
+/* ESERCIZIO 3
+ Scriti il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+
+/* console.log (12 + 20); */
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+/* let x = 12 */
+
+/* ESERCIZIO 5
+ Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+
+/* let name = "Luigi" */
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+
+/* console.log (4 - x); */
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ Infine, verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+ NON HAI BISOGNO DI UN BLOCCO IF/ELSE. E' sufficiente utilizzare console.log().
+*/
+
+/* let name1 = "john";
+   let name2 = "John";
+   console.log (name1 == name2);
+
+   let name2 ="john"
+   console.log (name1=== name2);
+    */
